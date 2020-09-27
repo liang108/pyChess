effd08d5b62178464e95810aa7da127e6ef4039c
liang108 <70230350+liang108@users.noreply.github.com>
2020-09-27 01:54:08 -0700

    Updated piece classes
    Finished board with object representation, debugged class definitions
    Todo: Finish debugging class defs (self.moves [[]] notation, and fix error when checking empty square for color: ie., check if square is not empty then check its color)

--- chess.py
+++ chess.py
@@ -4,8 +4,14 @@ class Piece:
             raise Exception("Attempted to initialize with invalid team")
         self.row = init_row
         self.col = init_col
-        self.color = team
-        self.moves = [self.row, self.col]
+        self.color = team   
+        self.moves = [[self.row, self.col]]
+
+    def __str__(self):
+        return self.symbol
+
+    def __repr__(self):
+        return self.symbol
 
 
 class Pawn(Piece):
@@ -15,40 +21,45 @@ class Pawn(Piece):
         if team == "black":
             self.symbol = "p"
         Piece.__init__(self, init_row, init_col, team)
-
-    def __str__(self):
-        return self.symbol
     
     def CheckMoves(self, board):
-        self.moves = [self.row, self.col]
+        self.moves = [[self.row, self.col]]
 
         # We want to build a list of possible positions for the pawn to move to
 
         if self.color == "white":
             if (self.row + 1 <= 7):                # If pawn is on furthest row then no moves are possible
-                if (board[self.row+1][self.col] == 0):      # Basic forward move is possible if square there is empty and is not on edge of board
-                    self.moves = self.moves + [self.row+1, self.col]
+                if (board.matrix[self.row+1][self.col] == 0):      # Basic forward move is possible if square there is empty and is not on edge of board
+                    self.moves = self.moves + [[self.row+1, self.col]]
                 if (self.col + 1 <= 7):            # Check if piece not on right edge 
-                    if (board[self.row+1][self.col+1] != 0): # Check if right diagonal square is not empty. Need previous condition so we don't access outside array
-                        self.moves = self.moves + [self.row+1, self.col+1]
+                    if (board.matrix[self.row+1][self.col+1] != 0): # Check if right diagonal square is not empty. Need previous condition 
+                        if (board.matrix[self.row+1][self.col+1].color != self.color):
+                            self.moves = self.moves + [[self.row+1, self.col+1]]
                 if (self.col - 1 >= 0):
-                    if (board[self.row+1][self.col-1] != 0):    
-                        self.moves = self.moves + [self.x-1, self.y+1]
-
-            return self.moves
+                    if (board.matrix[self.row+1][self.col-1] == 0):
+                        self.moves = self.moves + [[self.row+1, self.col-1]]
+                    if (board.matrix[self.row+1][self.col-1] != 0):
+                        if (board.matrix[self.row+1][self.col-1].color != self.color):
+                            self.moves = self.moves + [[self.row+1, self.col-1]]
 
         if self.color == "black":
             if (self.row - 1 >= 0):
-                if (board[self.row-1][self.col] == 0):
-                    self.moves = self.moves + [self.row-1, self.col]
+                if (board.matrix[self.row-1][self.col] == 0):
+                    self.moves = self.moves + [[self.row-1, self.col]]
             if (self.col - 1 >= 0):     # Right diagonal move is possible if square nonempty (from perspective of player with black pieces)
-                if (board[self.row-1][self.col-1] != 0): 
-                    self.moves = self.moves + [self.row-1, self.col-1]
+                if (board.matrix[self.row-1][self.col-1] == 0): 
+                    self.moves = self.moves + [[self.row-1, self.col-1]]
+                if (board.matrix[self.row-1][self.col-1] != 0):
+                    if (board.matrix[self.row-1][self.col-1].color != self.color):
+                        self.moves = self.moves + [[self.row-1, self.col-1]]
             if (self.col + 1 <= 7):     # Left diagonal move is possible if square nonempty
-                if (board[self.row-1][self.col+1] != 0 ):
-                    self.moves = self.moves + [self.row-1, self.col+1]
+                if (board.matrix[self.row-1][self.col+1] == 0):
+                    self.moves = self.moves + [[self.row-1, self.col+1]]
+                if (board.matrix[self.row-1][self.col+1] != 0):
+                    if (board.matrix[self.row-1][self.col+1].color != self.color ):
+                        self.moves = self.moves + [[self.row-1, self.col+1]]
             
-            return self.moves
+        return self.moves
 
 
 class Rook(Piece):
@@ -59,34 +70,32 @@ class Rook(Piece):
             self.symbol = "r"
         Piece.__init__(self, init_row, init_col, team)
 
-    def __str__(self):
-        return self.symbol
 
     def CheckMoves(self, board):
-        self.moves = [self.row, self.col]
+        self.moves = [[self.row, self.col]]
 
         # Check vertical moves
 
         if self.row != 7: 
             for i in range(self.row + 1, 8):    # Starting from rook, check squares above rook to see if empty. If a square is not empty, then rook cannot move past it
                 if board.matrix[i][self.col] == 0:
-                    self.moves = self.moves + [i, self.col]
+                    self.moves = self.moves + [[i, self.col]]
                 else:
                     if board.matrix[i][self.col].color == self.color:
                         break
-                    self.moves = self.moves + [i, self.col]
+                    self.moves = self.moves + [[i, self.col]]
                     break
 
         if self.row != 0:      # Now check the squares below the rook, starting from the rook and going down
             i = self.row - 1
             while (i >= 0):
                 if board.matrix[i][self.col] == 0:
-                    self.moves = self.moves + [i, self.col]
+                    self.moves = self.moves + [[i, self.col]]
                     i = i-1
                 else:
                     if board.matrix[i][self.col].color == self.color:
                         break
-                    self.moves = self.moves + [i, self.col]
+                    self.moves = self.moves + [[i, self.col]]
                     break
 
         # Check horizontal moves
@@ -94,24 +103,97 @@ class Rook(Piece):
         if self.col != 7:
             for i in range(self.col, 8):
                 if board.matrix[self.row][i] == 0:
-                    self.moves = self.moves + [self.row, i]
+                    self.moves = self.moves + [[self.row, i]]
                 else:
                     if board.matrix[self.row][i].color == self.color:
                         break
-                    self.moves = self.moves + [self.row, i]
+                    self.moves = self.moves + [[self.row, i]]
                     break
 
         if self.col != 0:
             i = self.col - 1
             while (i>=0):
                 if board.matrix[self.row][i] == 0:
-                    self.moves = self.moves + [self.row, i]
+                    self.moves = self.moves + [[self.row, i]]
                     i = i-1
                 else:
                     if board.matrix[self.row][i].color == self.color:
                         break
-                    self.moves = self.moves + [self.row, i]
+                    self.moves = self.moves + [[self.row, i]]
                     break
+
+        return self.moves
+
+
+class Knight(Piece):
+    def __init__(self, init_row, init_col, team):
+        if team == "white":
+            self.symbol = "Kn"
+        if team == "black":
+            self.symbol = "kn"
+        Piece.__init__(self, init_row, init_col, team)
+
+    def CheckMoves(self, board):
+        self.moves = [[self.row, self.col]]
+
+        if (self.row + 2 <= 7):
+            if (self.col - 1 >= 0):
+                if (board.matrix[self.row+2][self.col-1] == 0):
+                    self.moves = self.moves + [[self.row+2, self.col-1]]
+                else:
+                    if (board.matrix[self.row+2][self.col-1].color != self.color):
+                        self.moves = self.moves + [[self.row+2, self.col-1]]
+
+            if (self.col + 1 <= 7):
+                if (board.matrix[self.row+2][self.col+1] == 0):
+                    self.moves = self.moves + [[self.row+2, self.col+1]]
+                else:
+                    if (board.matrix[self.row+2][self.col+1].color != self.color):
+                        self.moves = self.moves + [[self.row+2, self.col+1]]
+
+        if (self.row + 1 <= 7):
+            if (self.col - 2 >= 0):
+                if (board.matrix[self.row+1][self.col-2] == 0):
+                    self.moves = self.moves + [[self.row+1, self.col-2]]
+                else:
+                    if (board.matrix[self.row+1][self.col-2].color != self.color):
+                        self.moves = self.moves + [[self.row+1, self.col-2]]
+
+            if (self.col + 2 <= 7):
+                if (board.matrix[self.row+1][self.col+2] == 0):
+                    self.moves = self.moves + [[self.row+1, self.col+2]]
+                else:
+                    if (board.matrix[self.row+1][self.col+2].color != self.color):
+                        self.moves = self.moves + [[self.row+1, self.col+2]]
+
+        if (self.row - 1 >= 0):
+            if (self.col - 2 >=0):
+                if (board.matrix[self.row-1][self.col-2] == 0):
+                    self.moves = self.moves + [[self.row-1, self.col-2]]
+                else:
+                    if (board.matrix[self.row-1][self.col-2].color != self.color):
+                        self.moves = self.moves + [[self.row-1, self.col-2]]
+
+            if (self.col + 2 <= 7):
+                if (board.matrix[self.row-1][self.col+2] == 0):
+                    self.moves = self.moves + [[self.row-1, self.col+2]]
+                else:    
+                    if (board.matrix[self.row-1][self.col+2].color != self.color):
+                        self.moves = self.moves + [[self.row-1, self.col+2]]
+        
+        if (self.row - 2 >= 0):
+            if (self.col - 1 >= 0):
+                if (board.matrix[self.row-2][self.col-1] == 0):
+                    self.moves = self.moves + [[self.row-2, self.col-1]]
+                else:
+                    if (board.matrix[self.row-2][self.col-1].color != self.color):
+                        self.moves = self.moves + [[self.row-2, self.col-1]]
+            if (self.col + 1 <= 7):
+                if (board.matrix[self.row+2][self.col+1] == 0):
+                    self.moves = self.moves + [[self.row-2, self.col+1]]
+                else:
+                    if (board.matrix[self.row+2][self.col+1].color != self.color):
+                        self.moves = self.moves + [[self.row-2, self.col+1]]
 
         return self.moves
 
@@ -124,10 +206,10 @@ class Bishop(Piece):
             self.symbol = "b"
         Piece.__init__(self, init_row, init_col, team)
 
-    def __str__(self):
-        return self.symbol
 
     def CheckMoves(self, board):
+        self.moves = [[self.row, self.col]]
+
         if self.row != 7:   # Check upper diagonals
             if self.col != 0:   # Check up left diagonal
                 i = self.row + 1
@@ -199,11 +281,9 @@ class Queen(Piece):
             self.symbol = "q"
         Piece.__init__(self, init_row, init_col, team)
 
-    def __str__(self):
-        return self.symbol
 
     def CheckMoves(self, board):
-        self.moves = [self.row, self.col]
+        self.moves = [[self.row, self.col]]
 
         # Check vertical moves
 
@@ -315,6 +395,7 @@ class Queen(Piece):
             
         return self.moves
 
+
 class King(Piece):
     def __init__(self, init_row, init_col, team):
         if team == "white":
@@ -322,12 +403,11 @@ class King(Piece):
         if team == "black":
             self.symbol = "k"
         Piece.__init__(self, init_row, init_col, team)
-
-    def __str__(self):
-        return self.symbol
+        
 
     def CheckMoves(self, board):
-        self.moves = [self.row, self.col]
+        self.moves = [[self.row, self.col]]
+
 
         if self.row != 7:   # Check moves above
             if self.col != 0:
@@ -353,28 +433,61 @@ class King(Piece):
                 if (board[self.row-1][self.col-1] == 0):
                     self.moves = self.moves + [self.row-1, self.col-1]
                 else:
-                    if(board[self.row-1][self.col-1].color != self.color):
+                    if (board[self.row-1][self.col-1].color != self.color):
                         self.moves = self.moves + [self.row-1, self.col-1]
             if (board[self.row-1][self.col] != 0):
                 if (board[self.row-1][self.col].color != self.color):
                     self.moves = self.moves + [self.row-1, self.col]
             if (board[self.row-1][self.col == 0]):
                 self.moves = self.moves + [self.row-1, self.col]
+            if self.col != 7:
+                if (board[self.row-1][self.col+1] == 0):
+                    self.moves = self.moves + [self.row-1, self.col+1]
+                else:
+                    if (board[self.row-1][self.col+1].color != self.color):
+                        self.moves = self.moves + [self.row-1, self.col+1]
 
-            # TODO: BOTTOM RIGHT SQUARE, LEFT AND RIGHT SQUARES
-    
+        if self.col != 0:   # Check left square
+            if (board[self.row][self.col-1] == 0):
+                self.moves = self.moves + [self.row, self.col-1]
+            else: 
+                if (board[self.row][self.col-1].color != self.color):
+                    self.moves = self.moves + [self.row, self.col-1]
+        
+        if self.col != 7:   # Check right square
+            if (board[self.row][self.col+1] == 0):
+                self.moves = self.moves + [self.row, self.col+1]
+            else:
+                if (board[self.row][self.col+1].color != self.color):
+                    self.moves = self.moves + [self.row, self.col+1]
+        
+        return self.moves
+
 
 class Board: 
-    # board is an 8x8 matrix. Use lists since you want to be able to change values of squares
-    # board[i][j] To access a square. i represents the row (file), and j represents the column (rank)
-    # board[0][0] would be the bottom left square from the perspective of the player with white pieces 
-    # board[7][7] would be the bottom left square from the perspective of the player with black pieces
-    # board[4][3] would be "d5" in algebraic chess representation
-    # Empty spaces are represented with 0
     def __init__(self):
-        #TODO: rewrite this with object representations instead of strings, then write string representation
-        self.matrix = [["R", "Kn", "B", "Q", "K", "B", "Kn", "R"], ["P", "P", "P", "P", "P", "P", "P", "P"], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], ["p", "p", "p", "p", "p", "p", "p", "p"], ["r", "kn", "b", "q", "k", "b", "kn", "r"]]
+        # Create pieces
 
+        self.pieces = [Rook(0,0,"white"), Knight(0,1,"white"), Bishop(0,2,"white"), Queen(0,3,"white"), King(0,4,"white"), Bishop(0,5,"white"), Knight(0,6,"white"), Rook(0,7,"white")]
+
+        for i in range(0,8):
+            self.pieces.append(Pawn(1,i,"white"))
+
+        for i in range(0,8):
+            self.pieces.append(Pawn(6,i,"black"))
+
+        self.pieces = self.pieces + [Rook(7,0,"black"), Knight(7,1,"black"), Bishop(7,2,"black"), Queen(7,3,"black"), King(7,4,"black"), Bishop(7,5,"black"), Knight(7,6,"black"), Rook(7,7,"black")]
+
+        # Initialize board, and place pieces on board
+
+        self.matrix = []
+        for i in range(0,8):
+            self.matrix.append([0,0,0,0,0,0,0,0])
+
+        for piece in self.pieces:
+            self.matrix[piece.row][piece.col] = piece
+
+        #self.matrix = [["R", "Kn", "B", "Q", "K", "B", "Kn", "R"], ["P", "P", "P", "P", "P", "P", "P", "P"], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], ["p", "p", "p", "p", "p", "p", "p", "p"], ["r", "kn", "b", "q", "k", "b", "kn", "r"]]
 
     def Display(self):
         new_matrix = self.matrix[:]
@@ -391,25 +504,30 @@ class Board:
         black_king = False 
 
         for row in self.matrix:
-            if("K" in row): 
-                white_king = True
-            if("k" in row):
-                black_king = True
+            for piece in row:
+                if isinstance(piece, King) == True:
+                    if (piece.color == "white"):
+                        white_king = True
+                    if (piece.color == "black"):
+                        black_king = True
         
         if (white_king == True) and (black_king == True):
             return True
         else:
             return False
 
-    def MovePiece(self, piece, new_row, new_col):  # Return True if move was made, False if move was not possible
-        legal_moves = CheckMoves(piece)
+    def MovePiece(self, init_row, init_col, new_row, new_col):  # Return True if move was made, False if move was not possible
+        piece = self.matrix[init_row][init_col]
+        legal_moves = piece.CheckMoves(self)
+
         if [new_row, new_col] not in legal_moves:
             print("Not a valid move, try again.")
             return False
-        else:
-            self.matrix[piece.row][piece.col] = 0       
-            self.matrix[new_row][new_col] = piece.symbol 
+
+        else:   
+            self.matrix[new_row][new_col] = piece
+            self.matrix[piece.row][piece.col] = 0 
             piece.row = new_row
             piece.col = new_col
-            piece.position = [piece.row, piece.col]
+            piece.moves = [piece.row, piece.col]
             return True
